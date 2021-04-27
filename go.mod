d367b683f809c5471f89b975174d2ec6d16094a9
luyang6 <luyang6@vipkid.com.cn>
2021-04-27 10:43:08 +0800

    swag

--- go.mod
+++ go.mod
@@ -3,16 +3,28 @@ module GoProject
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/EDDYCJY/go-gin-example v0.0.0-20201228125222-28f372bf41f9 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
+	github.com/gin-gonic/gin v1.7.1 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-playground/validator/v10 v10.5.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/swaggo/gin-swagger v1.3.0 // indirect
+	github.com/swaggo/swag v1.7.0 // indirect
 	github.com/ugorji/go v1.2.5 // indirect
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/sys v0.0.0-20210426080607-c94f62235c83 // indirect
+	golang.org/x/tools v0.1.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	_ "GoProject/docs"
+	//_ "github.com/swaggo/gin-swagger/example/docs"
 
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 
 	config "GoProject/ConfigClass"
 	test "GoProject/TestClass"
@@ -32,7 +42,29 @@ type Login struct {
 	Password string `form:"password" json:"password" xml:"password" binding:"required"`
 }
 
+// @title Swagger Example API
+// @version 0.0.1
+// @description  This is a sample server Petstore server.
+// @BasePath /api/v1/
 func main() {
+
+	//main
+	TestMain()
+	//关机重启
+	//testShutdown()
+
+}
+
+// @Summary Add a new pet to the store
+// @Description get string by ID
+// @Accept  json
+// @Produce json
+// @Param   {"message": "恭喜你", "id": "123"}
+// @Success 200 {string} string	"ok"
+// @Failure 400 {object} web.APIError "We need ID!!"
+// @Failure 404 {object} web.APIError "Can not find ID"
+// @Router /api/v1/getJson
+func TestMain() {
 
 	gin.ForceConsoleColor()
 
@@ -43,7 +75,7 @@ func main() {
 	router := gin.Default()
 
 	// JSON 绑定示例 ({"user": "manu", "password": "123"})
-	router.POST("/loginJSON", func(c *gin.Context) {
+	router.POST("/api/v1/loginJSON", func(c *gin.Context) {
 		var json Login
 		if err := c.ShouldBindJSON(&json); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -58,8 +90,8 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 
 	})
-
-	router.POST("/loginXML", func(c *gin.Context) {
+	//xml 绑定示例
+	router.POST("/api/v1/loginXML", func(c *gin.Context) {
 		var xml Login
 		if err := c.ShouldBindXML(&xml); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -75,7 +107,7 @@ func main() {
 	})
 
 	// 绑定HTML表单的示例 (user=manu&password=123)
-	router.POST("/loginForm", func(c *gin.Context) {
+	router.POST("/api/v1/loginForm", func(c *gin.Context) {
 		var form Login
 		//这个将通过 content-type 头去推断绑定器使用哪个依赖。
 		if err := c.ShouldBind(&form); err != nil {
@@ -91,12 +123,48 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 
-	router.GET("/getJson", func(c *gin.Context) {
+	//get方法
+	router.GET("/api/v1/getJson", func(c *gin.Context) {
 
 		c.JSON(http.StatusOK, gin.H{"message": "恭喜你", "id": "123"})
 		fmt.Println("请求成功了")
 	})
 
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":8080")
+}
+
+func testShutdown() {
+
+	router := gin.Default()
+	router.GET("/shutdown", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
+		c.String(http.StatusOK, "Welcome Gin Server")
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 
 }
